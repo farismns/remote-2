889a6271b2e5ed427f2f9354db39744a1c8de6ff
Asus <faris.m.saputra@gmail.com>
2025-08-02 22:12:59 +0700

    membuat index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Staff</title>
+</head>
+<body>
+    <h1>Data Staff</h1>
+    <ul>
+        <li>Heru : 143324</li>
+        <li>Heri 132123</li>
+    </ul>
+</body>
+</html>
